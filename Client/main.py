282505ff2390630d0c80a0170f86e222adb65713
Kareem El-Giushy <kareem.64123@gmail.com>
2023-12-05 23:00:03 -0600

    Add Client App Logic

--- Client/main.py
+++ Client/main.py
@@ -1,12 +1,25 @@
-from flask import Flask
-import requests
+from flask import Flask, render_template, redirect, request
+import requests as re
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return
+  return "<p>Hello, This Is An External Application</p>"
 
-@app.route("/Callback")
+@app.route("/connect", methods=['GET', 'POST'])
+def connect():
+  if request.method == "POST":
+    client_name = ""
+    redirect_uri = ""
+    response_type = ""
+    scope = ""
+
+    return redirect(f"http://localhost:5000/prompt?client_name=${client_name}&redirect_uri={redirect_uri}&response_type={response_type}&scope={scope}")
+
+  if request.method == "GET":
+    return render_template("connect.html")
+
+@app.route("/callback")
 def callback():
-  AccessToken = requests.post("get token")
+  return ''
